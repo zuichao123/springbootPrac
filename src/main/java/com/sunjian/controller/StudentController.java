c47bcb1171b2bbca3de5af33e7f538eda6f323de
zuichao <zuichao123@163.com>
2020-03-28 18:11:09 +0800

    update in 2020-03-28 18:11:30

--- src/main/java/com/sunjian/controller/StudentController.java
+++ src/main/java/com/sunjian/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.sunjian.controller;
 import com.sunjian.entity.Student;
 import com.sunjian.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author sunjian
@@ -19,7 +18,15 @@ public class StudentController {
 
     @RequestMapping("/findById/{id}")
     @ResponseBody
-    public Student findById(@PathVariable("id") Integer id){
+    public Student findById(@PathVariable("id") Integer id) {
         return studentService.findById(id);
+    }
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PostMapping("/set")
+    public void set(@RequestBody Student student) {
+        redisTemplate.opsForValue().set("stu", student);
     }
 }
--- src/main/java/com/sunjian/entity/Student.java
+++ src/main/java/com/sunjian/entity/Student.java
@@ -2,12 +2,14 @@ package com.sunjian.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author sunjian
  * @date 2020/3/27 22:30
  */
 @Data
-public class Student {
+public class Student implements Serializable {
     private Integer id;
     private String name;
 }
