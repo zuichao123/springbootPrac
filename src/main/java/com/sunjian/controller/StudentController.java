8cd8faf5f78bd522dc61031e0dde965f3ee8ad9e
zuichao <zuichao123@163.com>
2020-03-30 09:06:01 +0800

    update in 2020-03-30 09:05:30

--- src/main/java/com/sunjian/controller/StudentController.java
+++ src/main/java/com/sunjian/controller/StudentController.java
@@ -11,22 +11,48 @@ import org.springframework.web.bind.annotation.*;
  * @author sunjian
  * @date 2020/3/27 22:36
  */
-@Controller
+@RestController
 public class StudentController {
-    @Autowired
-    private StudentService studentService;
-
-    @RequestMapping("/findById/{id}")
-    @ResponseBody
-    public Student findById(@PathVariable("id") Integer id) {
-        return studentService.findById(id);
-    }
+//    @Autowired
+//    private StudentService studentService;
+//
+//    @RequestMapping("/findById/{id}")
+//    @ResponseBody
+//    public Student findById(@PathVariable("id") Integer id) {
+//        return studentService.findById(id);
+//    }
 
     @Autowired
     private RedisTemplate redisTemplate;
 
+    /**
+     * 增加、修改
+     * @param student
+     */
     @PostMapping("/set")
     public void set(@RequestBody Student student) {
         redisTemplate.opsForValue().set("stu", student);
+    }
+
+    /**
+     * 查找
+     * @param key
+     * @return
+     */
+    @GetMapping("/get/{key}")
+    public Student get(@PathVariable("key") String key){
+        Student student = (Student) redisTemplate.opsForValue().get(key);
+        return student;
+    }
+
+    /**
+     * 删除
+     * @param key
+     * @return
+     */
+    @GetMapping("/delete/{key}")
+    public boolean delete(@PathVariable("key") String key){
+        redisTemplate.delete(key);
+        return redisTemplate.hasKey(key);
     }
 }
